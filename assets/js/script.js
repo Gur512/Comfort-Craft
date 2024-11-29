a95f1fbdcb1bcbc1bd940b4fc0745f110281f2c5
Gurpreet kaur <gromana2022@gmail.com>
2024-11-29 14:06:52 -0600

    Adding model and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,34 @@
 'use strict';
 
 import * as utils from "./utils.js";
+
+const modal = utils.select('.modal');
+const loginBtn = utils.select(".header-btn");
+const closeBtn = utils.select(".close");
+const login = utils.select('.login-btn');
+const email = utils.select('.input-1');
+const password = utils.select('.input-2');
+
+utils.listen ('click', loginBtn, () => {
+    modal.style.display = "block";
+});
+
+utils.listen('click', closeBtn, () => {
+    modal.style.display = "none";
+});
+
+utils.listen('click', window, (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
+utils.listen('click', login, (event) => {
+    if (email.value.length !== 0 && password.value.length !== 0 ) {
+        email.value = "";
+        password.value = "";
+        modal.style.display = "none";
+    }
+});
+
+
